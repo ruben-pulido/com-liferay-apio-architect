ec515f975465c6d696282a0dba6ec4a893de1248
Javier Gamarra <nhpatt@gmail.com>
2018-10-16 12:16:22 +0200

    APIO-244 sf

--- apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/writer/DocumentationWriter.java
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/writer/DocumentationWriter.java
@@ -38,12 +38,10 @@ import com.liferay.apio.architect.representor.Representor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
